6bd6157ab1555ec1c97148c5f73c78273769f6bf
2217263633 <45092465+2217263633@users.noreply.github.com>
2019-03-19 08:52:53 +0800

    Update js.js

--- js.js
+++ js.js
@@ -2,7 +2,7 @@
         var ss,str,str1,str2,idnum,url_tail;
         //该变量是取自 https://www.ysts8.com/play_1836_49_1_1.html 在这个页面里面有iframe,可以直接在firefox浏览器查看控制台，
         //对应格式复制黏贴即可，该网站一般一日更新多次，甚是麻烦，改日需要直接代码取iframe里面的内容，方可长期解决
-        url_tail="?1231703970736x1552894683x1231710101396-274b87a413438a4cfbc17fda608658bd?3";
+        url_tail="?101024254586831x1552956730x101024260717491-28873f3bdbe7b1b16f804ff43b54a633?1";
         function getname()
         {
              ss=document.getElementById("form").src;
